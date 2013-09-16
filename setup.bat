cdeae007336062fb0ade7a0693af29a282b78ab1
Matt McCormick <matt@mbmccormick.com>
2013-09-16 14:31:31 -0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,3 +4,5 @@ set /p name="Enter your application name: " %=%
 fart -r -a -i *.cs,*.xaml,*.xml,*.csproj "Bootstrap" %name%
 fart -f *.* "Bootstrap" %name%
 fart -r -a -i *.csproj,*.user,*.sln "Bootstrap" %name%
+
+del LICENSE.txt
